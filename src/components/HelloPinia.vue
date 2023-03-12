1bbc2a1838ccf75247369d06718e3e80f5e2ea26
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-12 14:42:46 +0900

    test

--- src/components/HelloPinia.vue
+++ src/components/HelloPinia.vue
@@ -1,4 +1,10 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter';
 
+const counter = useCounterStore()
+</script>
 <template>
   <div>Start Hello Pinia</div>
+  <div> {{ counter.count }}</div>
+  <div>{{ counter.doubleCount }}</div>
 </template>
