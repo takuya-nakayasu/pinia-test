7420263de156d3582531611d0a05f04b22c00195
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-12 14:05:55 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { useCounterStore } from './stores/counter';
+
+const counter = useCounterStore()
 </script>
 
 <template>
   <div>Hello</div>
+  <div>Count: {{ counter.count }}</div>
+  <div>DoubleCount: {{ counter.doubleCount }}</div>
+  <button>Increment</button>
 </template>
 
 <style scoped>
