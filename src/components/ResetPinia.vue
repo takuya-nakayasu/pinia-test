565c026c9d10cf128e37f8422bade11e20973a3b
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-19 23:00:20 +0900

    add patch

--- src/components/ResetPinia.vue
+++ src/components/ResetPinia.vue
@@ -3,14 +3,21 @@ import { storeToRefs } from 'pinia';
 import { useCounterStore2 } from '../stores/counter2';
 
 const counter = useCounterStore2()
-const { count, doubleCount } = storeToRefs(counter)
+const { count2, doubleCount } = storeToRefs(counter)
 const { increment } = counter
+
+const patch = () => {
+  counter.$patch({
+    count2: 100,
+  })
+}
 
 </script>
 <template>
   <h2>Reset Pinia</h2>
-  <div> {{ count }}</div>
+  <div> {{ count2 }}</div>
   <div>{{ doubleCount }}</div>
   <button @click="increment">UP</button>
   <button @click="counter.$reset">Reset</button>
+  <button @click="patch">Patch</button>
 </template>
