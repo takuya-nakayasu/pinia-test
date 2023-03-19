8db64818c832bf3e183d0557530ed113c3827d6f
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-19 18:22:14 +0900

    update

--- src/components/HelloPinia.vue
+++ src/components/HelloPinia.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import { useCounterStore } from '../stores/counter';
 
 const counter = useCounterStore()
+const { count, doubleCount } = storeToRefs(counter)
+const { increment } = counter
+
 </script>
 <template>
   <div>Start Hello Pinia</div>
-  <div> {{ counter.count }}</div>
-  <div>{{ counter.doubleCount }}</div>
+  <div> {{ count }}</div>
+  <div>{{ doubleCount }}</div>
+  <button @click="increment">UP</button>
 </template>
