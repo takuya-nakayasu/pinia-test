198e8711213ddfe56f1cd9101f735b0a9c6f3659
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-19 18:44:58 +0900

    fix

---
+++ src/stores/counter2.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter2', {
+    state: () => ({ count2: 0, name2: 'Eduardo' }),
+    getters: {
+      doubleCount: (state) => state.count2 * 2,
+    },
+    actions: {
+        increment() {
+            this.count2++
+        }
+    }
+})
